96adaf0c91f2a7c938c8f3dafee4613b4712d743
Dat Nguyen <dat@healint.com>
2019-03-14 23:53:30 +1030

    first commit

---
+++ tictactoegame/app/src/main/java/com/flinders/nguy1025/tictactoe/GameManager.kt
@@ -0,0 +1,2 @@
+package com.flinders.nguy1025.tictactoe
+
---
+++ tictactoegame/app/src/main/java/com/flinders/nguy1025/tictactoe/MainActivity.kt
@@ -0,0 +1,51 @@
+package com.flinders.nguy1025.tictactoe
+
+import android.os.Bundle
+import android.support.design.widget.Snackbar
+import android.support.v7.app.AppCompatActivity;
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.TableLayout
+import android.widget.TextView
+
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    // Define variables
+    var gameBoard : Array<CharArray> = Array(3) { CharArray(3) }
+    var name: String = "Brett"
+    var tableLayout: TableLayout? = null
+    var turnTextView: TextView? = null
+    var turn : Char = 'X'
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        setSupportActionBar(toolbar)
+
+        fab.setOnClickListener { view ->
+            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                .setAction("Action", null).show()
+        }
+
+        var gameBoard : Array<CharArray> = Array(3) { CharArray(3) }
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        menuInflater.inflate(R.menu.menu_main, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        return when (item.itemId) {
+            R.id.action_settings -> true
+            else -> super.onOptionsItemSelected(item)
+        }
+    }
+}
